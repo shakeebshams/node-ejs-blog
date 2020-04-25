78d74448f858cb77d62002d80f06fe8666aec1dc
shakeebshams <shakeebshams@gmail.com>
2020-04-24 22:21:18 -0400

    completed website

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,8 +16,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = [];
+
 app.get('/', function(req, res) {
-  res.render('home', {startingContent: homeStartingContent});
+  res.render('home', 
+    {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+
+
 });
 
 app.get('/about', function(req, res) {
@@ -27,8 +36,35 @@ app.get('/contact', function(req, res) {
   res.render('contact', {contactContent: contactContent});
 });
 
+app.get('/post/:id', function(req, res) {
+  console.log(req.params.id);
+
+  var requestedTitle =  _.lowerCase(req.params.id);
+  posts.forEach(function(el) {
+    if (_.lowerCase(el.title) === requestedTitle) {
+      console.log("Match found!");
+      res.render('post', {title: el.title, content: el.content});
+    }
+  });
+});
+
 app.get('/compose', function(req, res) {
   res.render('compose', {});
+});
+
+app.post('/compose', function(req, res) {
+  console.log(req.body.postTitle);
+
+  let postResults = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(postResults);
+
+  res.redirect('/');
+
+
 });
 
 
